68389003856501776f49c0e608ff4d292736a5ed
MOHD NEHAL KHAN <nehalk482@gmail.com>
2025-04-27 15:42:48 +0530

    Added Trip Provider in main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
 import './index.css'
+import { TripProvider } from "./context/TripContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <TripProvider>
+        <App />
+      </TripProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
